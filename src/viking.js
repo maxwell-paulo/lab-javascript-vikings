f01018817e203bb9ef10fdd9125117632386ca8c
Maxwell Paulo <Maxwell@Maxwells-MacBook-Pro.local>
2022-08-15 16:59:51 -0300

    Final with bonus

--- src/viking.js
+++ src/viking.js
@@ -47,4 +47,44 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+
+  vikingAttack() {
+    let saxonDano = this.saxonArmy[0].receiveDamage(
+      this.vikingArmy[0].attack()
+    );
+    if (this.saxonArmy[0].health <= 0) {
+      this.saxonArmy.splice(0, 1);
+    }
+    return saxonDano;
+  }
+
+  saxonAttack() {
+    let vikingDano = this.vikingArmy[0].receiveDamage(
+      this.saxonArmy[0].attack()
+    );
+    if (this.vikingArmy[0].health <= 0) {
+      this.vikingArmy.splice(0, 1);
+    }
+    return vikingDano;
+  }
+
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
+}
